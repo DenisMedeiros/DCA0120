b383eb7d916e2923f1daa38ebe889d38d8227b9e
Denis Ricardo da Silva Medeiros <denis@dimap.ufrn.br>
2016-10-20 17:19:51 -0300

    Estrutura básica do projeto Web mesclada com as classes básicas.

---
+++ Projeto Final/Web/Digentrega/src/dca0120/servlets/InicioServlet.java
@@ -0,0 +1,13 @@
+package dca0120.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class InicioServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -999600673680107168L;
+	
+	
+	
+	
+
+}
