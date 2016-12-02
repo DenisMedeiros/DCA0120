029943141b671b24bda5568e6062f708fecc0622
Denis Medeiros <dnsricardo@gmail.com>
2016-12-02 11:40:22 -0300

    Dados do BD fora do código fonte.

--- ProjetoFinal/Web/Digentrega/src/dca0120/dao/ConnectionFactory.java
+++ ProjetoFinal/Web/Digentrega/src/dca0120/dao/ConnectionFactory.java
@@ -1,8 +1,11 @@
 package dca0120.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * @author denis
@@ -13,12 +16,9 @@ import java.sql.SQLException;
  */
 public class ConnectionFactory {
 	
-	//private final static String caminhoCompleto = "jdbc:h2:~/dados";jdbc:h2:tcp://localhost/~/banco
-	private final static String caminhoCompleto = "jdbc:h2:tcp://localhost/~/banco";
-	private final static String usuario = "usuario";
-	private final static String senha = "123";
-	
-	
+	//private final static String caminhoCompleto = "
+
+		
 	/** Abre a conexão com o banco de dados e retorna-a.
 	 *
 	 * @return conexao Conexão com o banco de dados.
@@ -26,9 +26,33 @@ public class ConnectionFactory {
 	 * @throws SQLException
 	 */
 	public static Connection getConexao() throws ClassNotFoundException, SQLException {
-		Class.forName("org.h2.Driver");
-		Connection conexao = DriverManager.getConnection(caminhoCompleto, usuario, senha);
-		return conexao;
+		
+		String driver;
+		String url;
+		String usuario;
+		String senha;
+		
+		InputStream is = ConnectionFactory.class.getClassLoader().getResourceAsStream("bd.properties");
+		
+		Properties prop = new Properties();
+	    try {
+			prop.load(is);
+		    
+		    driver = prop.getProperty("driver");
+		    url = prop.getProperty("url");
+		    usuario = prop.getProperty("usuario");
+		    senha = prop.getProperty("senha");
+		    
+			Class.forName(driver);
+			Connection conexao = DriverManager.getConnection(url, usuario, senha);
+			return conexao;
+		    
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    
+		return null;
+
 		
 	}
 }
